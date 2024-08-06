dca717c19e22fbf5e3c97f914aa98c5737c4b177
rnandory <tntjs2637@gmail.com>
2024-08-06 15:28:11 +0900

    0806 DispatcherServlet, IndexController implments Controller completed

--- src/main/java/com/newlecture/web/controller/DispatcherServlet.java
+++ src/main/java/com/newlecture/web/controller/DispatcherServlet.java
@@ -4,10 +4,13 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Map;
 import java.util.Properties;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
 
 import jakarta.servlet.ServletConfig;
@@ -20,20 +23,20 @@ import jakarta.servlet.http.HttpServletResponse;
 
 @WebServlet(urlPatterns = "/", loadOnStartup = 1, initParams = {
 		@WebInitParam(name = "path", value = "/WEB-INF/app.properties"),
-		@WebInitParam(name = "spring", value = "/WEB-INF/spring-dispatcher.xml")
-})
-public class DispatcherServlet extends HttpServlet{
-	
+		@WebInitParam(name = "spring", value = "/WEB-INF/spring-dispatcher.xml") })
+public class DispatcherServlet extends HttpServlet {
+
 //	private Properties props;
 	private ApplicationContext context;
-	
+
 	@Override
 	public void init(ServletConfig config) throws ServletException {
-		
+
 		String path = config.getInitParameter("spring");
 		String realPath = config.getServletContext().getRealPath(path);
-		context = new ClassPathXmlApplicationContext(realPath);
-		
+//		context = new ClassPathXmlApplicationContext("spring-dispatcher.xml");
+		context = new FileSystemXmlApplicationContext(realPath);
+
 //		// spring없이 props로 url-controller클래스 맵핑
 //		props = new Properties();
 //		String path = config.getInitParameter("path");
@@ -52,26 +55,28 @@ public class DispatcherServlet extends HttpServlet{
 //			// TODO Auto-generated catch block
 //			e.printStackTrace();
 //		}
-		
+
 	}
-	
+
 	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		String uri = req.getRequestURI();
-		Controller controller = (Controller) context.getBean(uri);
-		
-		// 요청한 url이 설정목록에 없을 때
-		if (controller == null) {
+
+		try {
+			Controller controller = (Controller) context.getBean(uri);
+
+			ModelAndView mv = controller.handleRequest(req, resp);
+
+			for (Map.Entry<String, Object> en : mv.getModel().entrySet())
+				req.setAttribute(en.getKey(), en.getValue());
+
+			req.getRequestDispatcher(mv.getViewName()).forward(req, resp);
+
+		} catch (Exception e) {
 			resp.sendError(HttpServletResponse.SC_NOT_FOUND);
 			return;
 		}
-		
-		try {
-			controller.handleRequest(req, resp);
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+
 	}
-	
+
 }
--- src/main/java/com/newlecture/web/controller/IndexController.java
+++ src/main/java/com/newlecture/web/controller/IndexController.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.web.context.support.WebApplicationContextUtils;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
 
 import com.newlecture.web.entity.Exam;
 import com.newlecture.web.service.ExamService;
@@ -17,26 +19,37 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 //@WebServlet("/index")
-public class IndexController extends HttpServlet {
-
+public class IndexController /* extends HttpServlet */ implements Controller{
+	
 	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		ServletContext servletContext = req.getServletContext();
-//		ApplicationContext context = (ApplicationContext) servletContext.getAttribute("context");
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		
-		ApplicationContext context 
-		= WebApplicationContextUtils.getWebApplicationContext(getServletContext());
-
+		ModelAndView mv = new ModelAndView("/WEB-INF/view/index.jsp");
+		// mv.setViewName("/WEB-INF/view/index.jsp");
 		
-		ExamService service = context.getBean(ExamService.class);
-
-		List<Exam> list;
-		try {
-			list = service.getList();
-			System.out.println(list);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		mv.addObject("test", "Hello~");
+		
+		return mv;
 	}
+
+//	@Override
+//	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//		ServletContext servletContext = req.getServletContext();
+////		ApplicationContext context = (ApplicationContext) servletContext.getAttribute("context");
+//		
+//		ApplicationContext context 
+//		= WebApplicationContextUtils.getWebApplicationContext(getServletContext());
+//
+//		
+//		ExamService service = context.getBean(ExamService.class);
+//
+//		List<Exam> list;
+//		try {
+//			list = service.getList();
+//			System.out.println(list);
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//	}
 }
